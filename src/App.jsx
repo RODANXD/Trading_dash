bfa8b88b041acc633c420085a895a5f3bb0967af
ankush negi <ankunegi26@email.com>
2024-09-08 18:09:51 +0530

    ye naya add kia h

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-import { useState } from 'react'
-import Design from './components/Design'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { MantineProvider } from "@mantine/core";
+import Design from './components/Design';
 import Custom from './components/Dashboard1';
 import Strategy from './components/Strategy';
 import TradingForm from './components/Strategy2';
-import { MantineProvider } from "@mantine/core";
 import { ChartAreaStacked } from './components/Home';
 import LoginPage from './components/login';
 
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
     <MantineProvider>
       <Router>
         <Routes>
-            <Route path="/" element={<Design/>}>
-            <Route path="/login" element={<LoginPage />} />
-
-            <Route path="/Strategy" element={<Custom />} />
-            <Route path="/home" element={<ChartAreaStacked />} />
-            <Route path="/Strategy2" element={<Strategy />} />
-            <Route path="/Strategy3" element={<TradingForm/>} />
-
-            
-            </Route>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<Design />}>
+            <Route path="home" element={<ChartAreaStacked />} />
+            <Route path="strategy" element={<Custom />} />
+            <Route path="strategy2" element={<Strategy />} />
+            <Route path="strategy3" element={<TradingForm />} />
+          </Route>
         </Routes>
       </Router>
-      </MantineProvider>
-
-    </>
-  )
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
