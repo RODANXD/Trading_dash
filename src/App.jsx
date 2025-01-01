5c3ab2eaa85c7254de7b9a8aa734269107f2b86f
ankush negi <ankunegi26@email.com>
2025-01-02 00:12:55 +0530

    edge_browser update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from "@mantine/core";
 import Design from './components/Design';
 import Custom from './components/Dashboard1';
 import Strategy from './components/Strategy';
 import TradingForm from './components/Strategy2';
 import Viewlegtable from './components/Viewlegtable';
-import {ChartLineInteractive} from './components/Home';
+import { ChartLineInteractive } from './components/Home';
 import DashTable from './components/DashTable';
 import Report from './components/Report';
 import LoginPage from './components/login';
 
 import './App.css';
 
+// Function to detect Microsoft Edge
+const useEdgeDetection = () => {
+  useEffect(() => {
+    if (navigator.userAgent.includes("Edg")) {
+      document.documentElement.classList.add("edge-browser"); // Add class for Edge browser
+    } else {
+      document.documentElement.classList.remove("edge-browser"); // Remove if not Edge
+    }
+  }, []);
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  useEdgeDetection(); // Call Edge detection
 
   return (
     <MantineProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />}/>
+          <Route path="/" element={<LoginPage />} />
           <Route path="/" element={<Design />}>
             <Route path="home" element={<ChartLineInteractive />} />
             <Route path="strategy" element={<Custom />} />
